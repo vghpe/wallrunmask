9457dc3ed92b47c08bdc73ec71be83d9ccc3fdba
Ragnar Rally <davidwideo@gmail.com>
2026-01-31 22:31:07 +0100

    Not shuffled, masks instead highlighted.

--- Assets/Scripts/MaskControl.cs
+++ Assets/Scripts/MaskControl.cs
@@ -3,44 +3,18 @@
 public class MaskControl : MonoBehaviour
 {
     [SerializeField] RectTransform[] masks;
-    int[] masksIndex = new int[3];
-    readonly int[] masksStored = new int[3] { (int)GameManager.colors.BLUE, (int)GameManager.colors.RED, (int)GameManager.colors.GREEN };
-    readonly int[] colorToMaskIndex = new int[3] {
-        1, //Red
-        2, //Green
-        0  //Blue
-    };
-    Vector3 middleVector;
+    [SerializeField] RectTransform icon;
+    //readonly int[] masksStored = new int[3] { (int)GameManager.colors.BLUE, (int)GameManager.colors.RED, (int)GameManager.colors.GREEN };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameManager.Singleton.ColorChangeEvent.AddListener(OnColorChange);
-        masksIndex[0] = (int)GameManager.colors.BLUE; //The left one
-        masksIndex[1] = (int)GameManager.colors.RED; // The middle one
-        masksIndex[2] = (int)GameManager.colors.GREEN; // The right one
-        middleVector = masks[1].position;
     }
 
     void OnColorChange()
     {
-        int maskMiddleColorIndex = masksIndex[1];
         int currentColor = (int)GameManager.Singleton.currentColor;
-        if (currentColor == maskMiddleColorIndex)
-        {
-            return;
-        }
-        int i = System.Array.IndexOf(masksStored, currentColor);
-        masks[colorToMaskIndex[maskMiddleColorIndex]].position = masks[i].position;
-        masks[i].position = middleVector;
-        masksIndex[1] = currentColor;
-        if (masksIndex[0] == currentColor)
-        {
-            masksIndex[0] = maskMiddleColorIndex;
-        }
-        else
-        {
-            masksIndex[2] = maskMiddleColorIndex;
-        }
+        icon.anchoredPosition = masks[currentColor].anchoredPosition;
     }
 }
