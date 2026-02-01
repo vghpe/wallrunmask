5a00c62bd94aff4afadf7ab6a53f9fdc6b16323c
Ragnar Rally <davidwideo@gmail.com>
2026-02-01 15:51:18 +0100

    Animation of figure.

---
+++ Assets/Scripts/AnimateCharacter.cs
@@ -0,0 +1,50 @@
+using StarterAssets;
+using UnityEngine;
+
+public class AnimateCharacter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private FirstPersonController firstPersonController;
+    Animator animationController;
+    void Start()
+    {
+        animationController = GetComponent<Animator>();
+        GameManager.Singleton.ColorChangeEvent.AddListener(ChangeMask);
+        firstPersonController = transform.parent.GetComponent<FirstPersonController>();
+    }
+
+    private bool wasWallRunning = false;
+    private int lastWallSide = 0;
+
+    private void Update()
+    {
+        bool isWallRunning = firstPersonController.WallRun;
+        int wallSide = firstPersonController.WallSide;
+
+        // Om wallrun-status ändrats
+        if (isWallRunning != wasWallRunning || wallSide != lastWallSide)
+        {
+            // Nollställ båda först (säkert)
+            animationController.SetBool("Left_Running", false);
+            animationController.SetBool("Right_Running", false);
+
+            if (isWallRunning)
+            {
+                if (wallSide == 1)
+                    animationController.SetBool("Right_Running", true);
+                else
+                    animationController.SetBool("Left_Running", true);
+            }
+
+            // Spara state
+            wasWallRunning = isWallRunning;
+            lastWallSide = wallSide;
+        }
+    }
+
+
+    void ChangeMask()
+    {
+        animationController.SetTrigger("ChangeMask");
+    }
+}
