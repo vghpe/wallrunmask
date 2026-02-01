72c7b892c5645903c239b8f8d14997865f70d12f
Henrik <gg02hpe@gmail.com>
2026-02-01 13:55:38 +0100

    Added shooting targets

--- Assets/ShootingTarget.cs
+++ Assets/ShootingTarget.cs
@@ -1,14 +1,21 @@
+using System;
 using UnityEngine;
 
 public class ShootingTarget : MonoBehaviour
 {
     public GameObject activationTarget;
 
+    // Event that fires when this target is hit
+    public event Action<ShootingTarget> onHit;
+
     public void OnHit()
     {
+        // Broadcast the hit event before deactivating
+        onHit?.Invoke(this);
+
         gameObject.SetActive(false);
 
-        if (activationTarget != null )
+        if (activationTarget != null)
         {
             activationTarget.GetComponent<ActivatedObjectParent>().OnActivation();
         }
